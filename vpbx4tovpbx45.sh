41475f7f0a39f64ebd0f9295b75e4b6dd4ed450f
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2024-11-23 09:10:24 -0600

    Update vpbx4tovpbx45.sh

--- vpbx4tovpbx45.sh
+++ vpbx4tovpbx45.sh
@@ -40,40 +40,40 @@ vitalpbx check-integrity
 # Step 4: Remove Hotel Management Module (Deprecate in V4.5)
 apt remove vitalpbx-hotel-management -y
 
-# Step 5: Change repositories to Debian 12 (bookworm) and VitalPBBX
+# Step 5: Stop and disable apache2 service
+systemctl stop apache2.service
+systemctl disable apache2.service
+apt remove apache2 -y
+
+# Step 6: Change repositories to Debian 12 (bookworm) and VitalPBBX
 echo "Updating repositories to Debian 12..."
 sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
 sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list.d/*
 echo "Update VitalPBX Repo to V4.5:"
 sed -i 's/v4/v4.5/g' /etc/apt/sources.list.d/vitalpbx.list
 
-# Step 6: Upgrade to Debian 12
+# Step 7: Upgrade to Debian 12
 # Set non-interactive mode globally
 export DEBIAN_FRONTEND=noninteractive
 
-# Step 7: Prevent interactive prompts
+# Step 8: Prevent interactive prompts
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 echo "Updating the package list..."
 apt update -y
 
-# Step 8: Update and upgrade the system
+# Step 9: Update and upgrade the system
 echo "Updating and upgrading the system..."
 apt upgrade -yq
 apt dist-upgrade -yq
 
-# Step 9: Clean up residual packages
+# Step 10: Clean up residual packages
 echo "Removing residual packages..."
 apt autoremove --purge -y
 apt clean -y
 
-# Step 10: Update GRUB bootloader
+# Step 11: Update GRUB bootloader
 echo "Updating GRUB..."
 update-grub
-
-# Step 11: Stop and disable apache2 service
-systemctl stop apache2.service
-systemctl disable apache2.service
-apt remove apache2 -y
 
 # Step 12: Re-Install-Upgrade VitalPBX
 apt reinstall vitalpbx -y
